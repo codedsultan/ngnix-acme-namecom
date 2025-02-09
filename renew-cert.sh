6814cd608bb5c6a51ec7eb58483f4144d2896c7e
codesultan <codesultan369@gmail.com>
2025-02-09 15:32:52 +0100

    wip

--- renew-cert.sh
+++ renew-cert.sh
@@ -37,6 +37,16 @@ export Namecom_Token="${NAMECOM_TOKEN}"
 log_message "Starting certificate issuance/renewal processes"
 send_slack_notification ":hourglass: Starting SSL certificate issuance/renewal for xurl.fyi"
 
+log_message "Removing old DNS challenge records..."
+send_slack_notification ":hourglass: Removing old DNS challenge records..."
+
+$ACME_SH --remove-dns-dv -d xurl.fyi
+$ACME_SH --remove-dns-dv -d "*.xurl.fyi"
+
+# Ensure removal before proceeding
+log_message "Checking for existing _acme-challenge TXT records..."
+send_slack_notification ":hourglass: Checking for existing _acme-challenge TXT records..."
+dig TXT _acme-challenge.xurl.fyi +short
 # Issue/renew the certificate
 if $ACME_SH --issue \
     --dns dns_namecom \
